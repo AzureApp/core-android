c928377f6c9145b82d3621988af86f07f21d59c7
Satori <comfycallum@gmail.com>
2018-12-31 19:57:43 +0000

    Formatting

--- src/core-android/logging.cc
+++ src/core-android/logging.cc
@@ -10,40 +10,43 @@
 
 // android implementation of logging function(s)
 
-#include <libazure/logging.h>
 #include <android/log.h>
+#include <libazure/logging.h>
 namespace azure {
 
 static int az_log_level_to_android(int level) {
-    switch (level) {
-        case AZ_LOG_INFO: return ANDROID_LOG_INFO;
-        case AZ_LOG_WARN: return ANDROID_LOG_DEBUG;
-        case AZ_LOG_ERROR:
-        default: return ANDROID_LOG_ERROR;
-    }
+  switch (level) {
+    case AZ_LOG_INFO:
+      return ANDROID_LOG_INFO;
+    case AZ_LOG_WARN:
+      return ANDROID_LOG_DEBUG;
+    case AZ_LOG_ERROR:
+    default:
+      return ANDROID_LOG_ERROR;
+  }
 }
 
 void WriteToLog(int level, const char *fmt, ...) {
-    va_list args;
-    va_start(args, fmt);
+  va_list args;
+  va_start(args, fmt);
 
-    __android_log_print(az_log_level_to_android(level), "Azure", fmt, args);
+  __android_log_print(az_log_level_to_android(level), "Azure", fmt, args);
 
-    if (!strstr(fmt, "\n")) {
-        fmt = concat(fmt, "\n");
-    }
-    const char *logger = "[Azure Daemon] ";
-    char result[256];
+  if (!strstr(fmt, "\n")) {
+    fmt = concat(fmt, "\n");
+  }
+  const char *logger = "[Azure Daemon] ";
+  char result[256];
 
-    strcpy(result, logger);
-    strcat(result, fmt);
+  strcpy(result, logger);
+  strcat(result, fmt);
 
-    FILE *log_file = fopen(AZURE_LOG_LOC, "a+");
-    
-    vprintf(result, args);
-    va_end(args);
+  FILE *log_file = fopen(AZURE_LOG_LOC, "a+");
 
-    fclose(log_file);
+  vprintf(result, args);
+  va_end(args);
+
+  fclose(log_file);
 }
 
-} // namespace azure
+}  // namespace azure
--- src/daemon/main.cc
+++ src/daemon/main.cc
@@ -14,9 +14,9 @@
 #include <gflags/gflags.h>
 #include <linenoise.h>
 
+#include <libazure/daemon.h>
 #include <libazure/data_objects/meta_object.h>
 #include <libazure/data_objects/search_object.h>
-#include <libazure/daemon.h>
 #include <libazure/logging.h>
 
 DEFINE_bool(daemon, false, "Run azure as a daemon");
@@ -26,25 +26,25 @@ DEFINE_int32(port, 1248, "Port for TCP server to connect to");
 namespace azure {
 
 extern "C" int main(int argc, char **argv) {
-    gflags::ParseCommandLineFlags(&argc, &argv, true);
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
 
-    if (FLAGS_daemon) {
-        AZLog("Starting azure in daemon mode");
-        Daemon daemon(argc, argv, FLAGS_ip, FLAGS_port);
-        return daemon.Run();
-    } else {
-        AZLog("Starting azure in command-line mode");
-        char *line;
-        while ((line = linenoise("azure> ")) != NULL) {
-            if (!strcmp(line, "quit")) {
-                break;
-            }
-            printf("You wrote: %s\n", line);
-            linenoiseFree(line);
-        }
+  if (FLAGS_daemon) {
+    AZLog("Starting azure in daemon mode");
+    Daemon daemon(argc, argv, FLAGS_ip, FLAGS_port);
+    return daemon.Run();
+  } else {
+    AZLog("Starting azure in command-line mode");
+    char *line;
+    while ((line = linenoise("azure> ")) != NULL) {
+      if (!strcmp(line, "quit")) {
+        break;
+      }
+      printf("You wrote: %s\n", line);
+      linenoiseFree(line);
     }
+  }
 
-    // msgpack_main(argc, argv);
+  // msgpack_main(argc, argv);
 }
 
-} // namespace azure
+}  // namespace azure
