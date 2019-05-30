e43a531e1248e23b41a6fd96262846cba5a348bd
Razzile <comfycallum@gmail.com>
2019-05-30 17:39:06 +0100

    Use core class to initialize grpc

--- src/daemon/main.cc
+++ src/daemon/main.cc
@@ -10,16 +10,12 @@
 
 #include <gflags/gflags.h>
 #include <grpc++/grpc++.h>
+#include <libazure/core.h>
 #include <libazure/services/connection_service.h>
 #include <linenoise.h>
 #include <iostream>
 #include <string>
 #include <vector>
-
-using grpc::Server;
-using grpc::ServerBuilder;
-using grpc::ServerContext;
-using grpc::Status;
 
 DEFINE_bool(daemon, true, "Run azure as a daemon");
 DEFINE_string(ip, "127.0.0.1", "IP address for TCP server to connect to");
@@ -33,23 +29,9 @@ extern "C" int main(int argc, char **argv) {
   if (FLAGS_daemon) {
     std::printf("This is a development executable. Please do not use\n");
 
-    std::string server_address("0.0.0.0:1248");
-    azure::ConnectionServiceImpl connection_service;
-
-    ServerBuilder builder;
-    // Listen on the given address without any authentication mechanism.
-    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-    builder.RegisterService(&connection_service);
-    // Register "service" as the instance through which we'll communicate with
-    // clients. In this case it corresponds to an *synchronous* service.
-    // builder.RegisterService(&service);
-    // Finally assemble the server.
-    std::unique_ptr<Server> server(builder.BuildAndStart());
-    std::cout << "Server listening on " << server_address << std::endl;
-
-    // Wait for the server to shutdown. Note that some other thread must be
-    // responsible for shutting down the server for this call to ever return.
-    server->Wait();
+    Core core("0.0.0.0");
+    core.AddService(new ConnectionServiceImpl());
+    core.Run();
 
     return 0;
   } else {
